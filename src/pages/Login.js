53261c856e552edfce0952726b073d9023369241
nbzImran <emran.nabizada@gmail.com>
2025-04-27 18:43:41 -0400

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:3001/auth/login", {
+      const res = await fetch("https://soccer-pickup-backend.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
